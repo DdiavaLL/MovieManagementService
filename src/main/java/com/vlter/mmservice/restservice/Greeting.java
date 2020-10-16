12bcd827a738ca141a4d2f219a694547a1c6a3eb
Tereshchenko Vlad <ddiavall@yandex.ru>
2020-10-17 00:27:52 +0300

    Initial commit

---
+++ src/main/java/com/vlter/mmservice/restservice/Greeting.java
@@ -0,0 +1,22 @@
+package com.vlter.mmservice.restservice;
+
+/**
+ * Created by Tereshchenko on 17.10.2020.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
