90c9b2f975716902e40f75712f7f87a3addc1fb4
Tereshchenko Vlad <ddiavall@yandex.ru>
2020-10-19 19:43:49 +0300

    Created a method for displaying information about a movie by id.

--- src/main/java/com/vlter/mmservice/restservice/MovieController.java
+++ src/main/java/com/vlter/mmservice/restservice/MovieController.java
@@ -5,22 +5,26 @@ package com.vlter.mmservice.restservice;
  */
 
 import java.util.List;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "/api/movies")
 public class MovieController {
     @Autowired
     private MovieRepository movieRepository;
     @Autowired
     private DirectorRepository directorRepository;
 
-    // Получить все записи
-    @GetMapping("/api/movies")
+    // Список всех кинофильмов
+    @GetMapping()
     public List getAllNotes() {
         return movieRepository.findAll();
     }
 
+    // Получить информацию о фильме по id
+    @GetMapping("/{id}")
+    public Movie getMovieById(@PathVariable (value = "id") Integer movieId) {
+        return movieRepository.findById(movieId).orElse(null);
+    }
 }
