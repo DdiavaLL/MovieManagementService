6af05c78f824533df801abd3894f57b3c2f21721
Tereshchenko Vlad <ddiavall@yandex.ru>
2020-10-21 00:16:04 +0300

    Created a method for adding a new movie.

--- src/main/java/com/vlter/mmservice/restservice/MovieController.java
+++ src/main/java/com/vlter/mmservice/restservice/MovieController.java
@@ -5,7 +5,11 @@ package com.vlter.mmservice.restservice;
  */
 
 import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,6 +19,8 @@ public class MovieController {
     private MovieRepository movieRepository;
     @Autowired
     private DirectorRepository directorRepository;
+
+    ObjectMapper objectMapper = new ObjectMapper();
 
     // Список всех кинофильмов
     @GetMapping()
@@ -27,4 +33,19 @@ public class MovieController {
     public Movie getMovieById(@PathVariable (value = "id") Integer movieId) {
         return movieRepository.findById(movieId).orElse(null);
     }
+
+    // Добавление записи о кинофильме
+    @RequestMapping(method = RequestMethod.POST)
+    public void submit(@ModelAttribute MovieWrapper movieWrapper, Model model) {
+        objectMapper.findAndRegisterModules();
+        Movie newMovie = null;
+        try {
+            newMovie = objectMapper.readValue(movieWrapper.getJson(), Movie.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        //movieRepository.save(newMovie);
+    }
+
+
 }
---
+++ src/main/java/com/vlter/mmservice/restservice/MovieWrapper.java
@@ -0,0 +1,16 @@
+package com.vlter.mmservice.restservice;
+
+/**
+ * Created by Tereshchenko on 20.10.2020.
+ */
+public class MovieWrapper {
+    private String json;
+
+    public String getJson() {
+        return json;
+    }
+
+    public void setJson(String json) {
+        this.json = json;
+    }
+}
